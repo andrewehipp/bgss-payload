336d625c81198a1eda67be2bb5a448629f2fd4eb
Andrew Hipp <andrew@havenagency.com>
2023-07-20 08:12:16 -0700

    chore: correct env variable names

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,9 @@ app.get('/', (_, res) => {
 });
 
 app.use(mediaManagement({ 
-  cloud_name: process.env.CLOUDFRONT_CLOUD_NAME, 
-  api_key: process.env.CLOUDFRONT_API_KEY, 
-  api_secret: process.env.CLOUDFRONT_API_SECRET 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 }));
           
 const start = async () => {
